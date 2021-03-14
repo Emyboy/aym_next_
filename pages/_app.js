fa472d3c529ce61fb767f4ab6685ab6a891aff1b
Emy Ifeora <emy@meetkumba.com>
2021-03-14 17:59:50 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,13 @@ import '../assets/css/vendor/bootstrap.min.css';
 import '../assets/css/vendor/slick.css'
 import '../assets/css/style.css';
 import '../assets/css/App.css';
+import Header from '../components/Header/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className='main-wrapper'>
+    <Header />
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,21 @@
-import Head from 'next/head'
-import Google from '../components/Google'
-import styles from '../styles/Home.module.css'
+import React, { useEffect } from 'react'
+import firebase from "firebase/app";
+import initFirebase from "../services/firebase";
 
-export default function Home() {
+initFirebase();
+
+
+export default function Index(props) {
+  console.log(props)
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-      <Google/>
     </div>
   )
 }
+
+// Index.getInitialProps = async (ctx) => {
+//   const data = [];
+//   const posts = await firebase.firestore().collection('posts').get()
+//   return { posts }
+// }
