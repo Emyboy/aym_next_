2e41df7b49d4e9493b0325f08997dd73aa437e7b
Emy Ifeora <emy@meetkumba.com>
2021-03-21 18:51:10 +0100

    ..

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,37 @@ initFirebase();
 export default function Index(props) {
   return (
     <div>
-      <NextSeo 
-        title='African Youth Minds'
-        description='Youths - Talent - Skill'
+      <NextSeo
+        title="African Youth Minds"
+        description="African Youth Minds"
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: 'https://www.url.ie/a',
+          title: 'African Youth Minds',
+          description: 'Open Graph Description',
+          images: [
+            {
+              url: 'https://firebasestorage.googleapis.com/v0/b/project-managemnt-a12b8.appspot.com/o/aym%2FAYM.png?alt=media&token=c0400067-a403-408d-a942-7c8e1b49a8e0',
+              width: 800,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+            {
+              url: 'https://firebasestorage.googleapis.com/v0/b/project-managemnt-a12b8.appspot.com/o/aym%2FAYM.png?alt=media&token=c0400067-a403-408d-a942-7c8e1b49a8e0',
+              width: 900,
+              height: 800,
+              alt: 'Og Image Alt Second',
+            },
+            { url: 'https://firebasestorage.googleapis.com/v0/b/project-managemnt-a12b8.appspot.com/o/aym%2FAYM.png?alt=media&token=c0400067-a403-408d-a942-7c8e1b49a8e0' },
+            { url: 'https://firebasestorage.googleapis.com/v0/b/project-managemnt-a12b8.appspot.com/o/aym%2FAYM.png?alt=media&token=c0400067-a403-408d-a942-7c8e1b49a8e0' },
+          ],
+          site_name: 'AfricanYouthMinds',
+        }}
+      // twitter={{
+      //     handle: '@handle',
+      //     site: '@site',
+      //     cardType: 'summary_large_image',
+      // }}
       />
       <Google />
       <div className='axil-post-list-area post-listview-visible-color axil-section-gap is-active'>
