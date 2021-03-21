65b0288075d626b347ef22d499d84b2ac75d0290
Emy Ifeora <emy@meetkumba.com>
2021-03-21 15:18:40 +0100

    ..

--- pages/_document.js
+++ pages/_document.js
@@ -17,9 +17,9 @@ class MyDocument extends Document {
                         name="description"
                         content="African Youth Minds"
                     />
-                    <link rel="icon" href="/public/img/logo.png" />
-                    <link rel="apple-touch-icon" href="/public/img/logo.png" />
-                    <link rel="manifest" href="/public/manifest.json" />
+                    <link rel="icon" href="/logo.png" />
+                    <link rel="apple-touch-icon" href="/logo.png" />
+                    <link rel="manifest" href="/manifest.json" />
                     <link
                         rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
